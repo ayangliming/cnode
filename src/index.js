85d6f22611850d504696fc917719b3f6f9d18ab1
黎明 <775391014@qq.com>
2017-07-17 17:36:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import 'antd/dist/antd.css';
 import "./main.css"
 import App from "./App.js"
 
-<<<<<<< HEAD
+
 import moment from "moment"
 moment.locale('zh-cn');
 
 console.log(moment(new Date()).fromNow())
-=======
->>>>>>> 1deac9089175c3cad4afd5e6f2eed5762791085f
+
+
 ReactDOM.render(<App />,document.getElementById('root'))
